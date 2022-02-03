fae4888ec27f79932f5d1c186b5b34a78281e6c7
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2022-02-03 18:27:39 -0300

    Colocando em prática

---
+++ Projeto_Insta/script.js
@@ -0,0 +1,24 @@
+ function instaname(){
+     let user = window.prompt("Para uma melhor experiência informe seu Instagram sem @");
+     let box_user = document.getElementById('user-name');
+     let not_user = document.getElementById('not-user');
+
+     box_user.innerHTML = `Continue como ${user}`
+     box_user.href = "https://www.instagram.com/"+user+"/";  
+     not_user.innerHTML = `Não é ${user}?`
+ }
+ 
+ 
+let phone = document.getElementById('phone')
+let num = 2
+
+function Background(){
+    phone.style.backgroundImage = 'url(img/Fundo'+num+'.png)';
+    num++
+
+    if (num == 7){
+        num = 1
+    }
+}
+
+setInterval(Background, 6000)
