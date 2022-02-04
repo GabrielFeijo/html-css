dca67426593e995d2adbccc3861c510337c77543
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2022-02-04 09:20:50 -0300

    Modificado

--- Projeto_Insta/script.js
+++ Projeto_Insta/script.js
@@ -1,7 +1,7 @@
  function instaname(){
-     let user = window.prompt("Para uma melhor experiência informe seu Instagram sem @");
-     let box_user = document.getElementById('user-name');
-     let not_user = document.getElementById('not-user');
+     var user = window.prompt("Para uma melhor experiência informe seu Instagram sem @");
+     var box_user = document.getElementById('user-name');
+     var not_user = document.getElementById('not-user');
 
      box_user.innerHTML = `Continue como ${user}`
      box_user.href = `https://www.instagram.com/${user}/`;  
